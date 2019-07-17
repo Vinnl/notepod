6ab33377bd644c3c1da1288c9cac677956bef4b6
Vincent <git@vincenttunru.com>
2019-07-17 13:40:31 +0200

    Adding a CSS framework
    This commit is not specific to building a Solid app, but it allows
    us to use the Bulma CSS framework: https://bulma.io
    
    This will allow us to use some basic styling without having to
    focus on it too much - distracting from the tutorial.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
     <React.StrictMode>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <p>This is a basic app that can use <a href="https://bulma.io/" title="The Bulma CSS framework">Bulma</a> for styling.</p>
     </React.StrictMode>
   );
 }
