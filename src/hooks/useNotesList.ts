inrupt/friend-requests-exploration

Setting up our data model
Now it's time to start working on some actual functionality: we'll
be making an app that allows people to keep notes. With this
commit, we'll prepare their Pod for our data model - much like how
you might prepare database tables in a traditional application
with a custom back-end.

At a high level, this involves the following steps:

1. Check if there exists a Document to track notes in.
2. If it does not exist, create it.
3. Fetch that Document.

So to start with the first step: in which Document should we track
notes? The answer lies in the concept of a _Public Type Index_.

The Public Type Index is itself a publicly accessible Document
stored in the user's Pod. This Document contains a list of links to
other Documents, along with the type of data that is to be included
in those Documents. To store notes, the data type we will use is
the `TextDigitalDocument`, defined by Schema.org [1]. Every
time the user saves a note, we will store it as a
TextDigitalDocument.

If the Document containing these notes was located in the
user's Pod at `/public/notes.ttl`, their Public Type Index could
refer to it like this:

| #notes | rdf:type       | solid:TypeRegistration     |
| #notes | solid:forClass | schema:TextDigitalDocument |
| #notes | solid:instance | /public/notes.ttl          |

The above Type Index includes one Type Registration, identified by
`#notes`, that registers `/public/notes.ttl` for the data type
`schema:TextDigitalDocument`.

So how do we find the user's Public Type Index? It's usually listed
in their profile, i.e. the Document accessible at their WebID:

| #me | solid:publicTypeIndex | /settings/publicTypeIndex.ttl |

This is a pattern you'll often encounter when writing Solid apps:
you start with the user's WebID, read the profile Document located
there, and from there you can find the data you need.

So that's exactly what we do in this commit. In the `<Dashboard>`
component, we want to list the number of movies the user has se

src/hooks/useNotesList.ts
@@ -1,0 +1,44 @@
+import React from 'react';
+import { fetchDocument, TripleDocument, TripleSubject } from 'tripledoc';
+import { solid, schema } from 'rdf-namespaces';
+import { usePublicTypeIndex } from './usePublicTypeIndex';
+import { initialiseNotesList } from '../services/initialiseNotesList';
+
+export function useNotesList() {
+  const publicTypeIndex = usePublicTypeIndex();
+  const [notesList, setNotesList] = React.useState<TripleDocument>();
+
+  React.useEffect(() => {
+    if (!publicTypeIndex) {
+      return;
+    }
+
+    (async () => {
+      const notesListIndex = publicTypeIndex.findSubject(solid.forClass, schema.TextDigitalDocument);
+      if (!notesListIndex) {
+        // If no watchlist document is listed in the public type index, create one:
+        const notesList = await initialiseNotesList()
+        if (notesList === null) {
+          return;
+        }
+        setNotesList(notesList);
+        return;
+      } else {
+        // If the public type index does list a watchlist document, fetch it:
+        const notesListUrl = notesListIndex.getNodeRef(solid.instance);
+        if (typeof notesListUrl !== 'string') {
+          return;
+        }
+        const document = await fetchDocument(notesListUrl);
+        setNotesList(document);
+      }
+    })();
+
+  }, [publicTypeIndex]);
+
+  return notesList;
+}
+
+export function getNotes(notesList: TripleDocument): TripleSubject[] {
+  return notesList.getSubjectsOfType(schema.TextDigitalDocument);
+}

src/services/addToTypeIndex.ts
@@ -1,0 +1,14 @@
+import { TripleDocument, NodeRef } from 'tripledoc';
+import { rdf, solid } from 'rdf-namespaces';
+
+export async function addToTypeIndex (
+  typeIndex: TripleDocument,
+  document: TripleDocument,
+  forClass: NodeRef,
+) {
+  const typeRegistration = typeIndex.addSubject();
+  typeRegistration.addNodeRef(rdf.type, solid.TypeRegistration)
+  typeRegistration.addNodeRef(solid.instance, document.asNodeRef())
+  typeRegistration.addNodeRef(solid.forClass, forClass)
+  return typeIndex.save([ typeRegistration ]);
+}

src/services/fetchPublicTypeIndex.ts
@@ -1,0 +1,18 @@
+import { fetchProfile } from './fetchProfile';
+import { fetchDocument } from 'tripledoc';
+import { solid } from 'rdf-namespaces';
+
+export async function fetchPublicTypeIndex () {
+  const profile = await fetchProfile();
+  if (profile === null) {
+    return null;
+  }
+
+  const publicTypeIndexUrl = profile.getNodeRef(solid.publicTypeIndex);
+  if (!publicTypeIndexUrl || typeof publicTypeIndexUrl !== 'string') {
+    return null;
+  }
+
+  const document = await fetchDocument(publicTypeIndexUrl);
+  return document;
+}

src/services/initialiseNotesList.ts
@@ -1,0 +1,26 @@
+import { fetchProfile } from './fetchProfile';
+import { space, schema } from 'rdf-namespaces';
+import { createDocument } from 'tripledoc';
+import { fetchPublicTypeIndex } from './fetchPublicTypeIndex';
+import { addToTypeIndex } from './addToTypeIndex';
+
+export async function initialiseNotesList () {
+  const [profile, publicTypeIndex] = await Promise.all([fetchProfile(), fetchPublicTypeIndex()]);
+  if (profile === null || publicTypeIndex === null) {
+    return null;
+  }
+
+  const storage = profile.getNodeRef(space.storage);
+  if (typeof storage !== 'string') {
+    return null;
+  }
+
+  // Note: There's an assumption here that `/public/` exists and is writable for this app.
+  //       In the future, "Shapes" should hopefully allow us to get more guarantees about this:
+  //       https://ruben.verborgh.org/blog/2019/06/17/shaping-linked-data-apps/#need-for-shapes
+  const notesListRef = storage + 'public/notes.ttl';
+  const notesList = createDocument(notesListRef);
+  await notesList.save();
+  await addToTypeIndex(publicTypeIndex, notesList, schema.TextDigitalDocument);
+  return notesList;
+}

Vinnl/notepod

src/hooks/useNotesList.ts
@@ -1,0 +1,44 @@
+import React from 'react';
+import { fetchDocument, TripleDocument, TripleSubject } from 'tripledoc';
+import { solid, schema } from 'rdf-namespaces';
+import { usePublicTypeIndex } from './usePublicTypeIndex';
+import { initialiseNotesList } from '../services/initialiseNotesList';
+
+export function useNotesList() {
+  const publicTypeIndex = usePublicTypeIndex();
+  const [notesList, setNotesList] = React.useState<TripleDocument>();
+
+  React.useEffect(() => {
+    if (!publicTypeIndex) {
+      return;
+    }
+
+    (async () => {
+      const notesListIndex = publicTypeIndex.findSubject(solid.forClass, schema.TextDigitalDocument);
+      if (!notesListIndex) {
+        // If no notes document is listed in the public type index, create one:
+        const notesList = await initialiseNotesList()
+        if (notesList === null) {
+          return;
+        }
+        setNotesList(notesList);
+        return;
+      } else {
+        // If the public type index does list a notes document, fetch it:
+        const notesListUrl = notesListIndex.getRef(solid.instance);
+        if (typeof notesListUrl !== 'string') {
+          return;
+        }
+        const document = await fetchDocument(notesListUrl);
+        setNotesList(document);
+      }
+    })();
+
+  }, [publicTypeIndex]);
+
+  return notesList;
+}
+
+export function getNotes(notesList: TripleDocument): TripleSubject[] {
+  return notesList.getSubjectsOfType(schema.TextDigitalDocument);
+}

src/services/addToTypeIndex.ts
@@ -1,0 +1,14 @@
+import { TripleDocument, Reference } from 'tripledoc';
+import { rdf, solid } from 'rdf-namespaces';
+
+export async function addToTypeIndex (
+  typeIndex: TripleDocument,
+  document: TripleDocument,
+  forClass: Reference,
+) {
+  const typeRegistration = typeIndex.addSubject();
+  typeRegistration.addRef(rdf.type, solid.TypeRegistration)
+  typeRegistration.addRef(solid.instance, document.asRef())
+  typeRegistration.addRef(solid.forClass, forClass)
+  return typeIndex.save([ typeRegistration ]);
+}

src/services/fetchPublicTypeIndex.ts
@@ -1,0 +1,18 @@
+import { fetchProfile } from './fetchProfile';
+import { fetchDocument } from 'tripledoc';
+import { solid } from 'rdf-namespaces';
+
+export async function fetchPublicTypeIndex () {
+  const profile = await fetchProfile();
+  if (profile === null) {
+    return null;
+  }
+
+  const publicTypeIndexUrl = profile.getRef(solid.publicTypeIndex);
+  if (!publicTypeIndexUrl || typeof publicTypeIndexUrl !== 'string') {
+    return null;
+  }
+
+  const document = await fetchDocument(publicTypeIndexUrl);
+  return document;
+}

src/services/initialiseNotesList.ts
@@ -1,0 +1,26 @@
+import { fetchProfile } from './fetchProfile';
+import { space, schema } from 'rdf-namespaces';
+import { createDocument } from 'tripledoc';
+import { fetchPublicTypeIndex } from './fetchPublicTypeIndex';
+import { addToTypeIndex } from './addToTypeIndex';
+
+export async function initialiseNotesList () {
+  const [profile, publicTypeIndex] = await Promise.all([fetchProfile(), fetchPublicTypeIndex()]);
+  if (profile === null || publicTypeIndex === null) {
+    return null;
+  }
+
+  const storage = profile.getRef(space.storage);
+  if (typeof storage !== 'string') {
+    return null;
+  }
+
+  // Note: There's an assumption here that `/public/` exists and is writable for this app.
+  //       In the future, "Shapes" should hopefully allow us to get more guarantees about this:
+  //       https://ruben.verborgh.org/blog/2019/06/17/shaping-linked-data-apps/#need-for-shapes
+  const notesListRef = storage + 'public/notes.ttl';
+  const notesList = createDocument(notesListRef);
+  await notesList.save();
+  await addToTypeIndex(publicTypeIndex, notesList, schema.TextDigitalDocument);
+  return notesList;
+}