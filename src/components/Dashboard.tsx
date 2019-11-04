inrupt/friend-requests-exploration

Accessing our first bits of data!
Now we're getting started! In this commit, we fetch our first bit
of data: the name of the current user. To explain how that works,
I'll need to give you a crash course on Linked Data - but don't
worry, I'll limit myself only to what is absolutely necessary to
understand what's going on.

In traditional back-ends, data is usually stored in database
tables. When your back-end is a Solid Pod, however, data is stored
in _Documents_ living at a certain URL - you can think of these as
being similar to HTML documents living at a URL.

When a person logs in to our app, we receive a _WebID_. A WebID can
be used both to identify that user -since no other user shares that
URL-, and as a pointer to a Document with information relevant to
that user.

My WebID is `https://vincentt.inrupt.net/profile/card#me`. That
means that there is a Document at
`https://vincentt.inrupt.net/profile/card` that contains data
relevant to me.

The most important thing to understand when writing a Solid app is
how that data is represented. In a nutshell, a Document contains a
list of relationships between things. For example, the Document
referred to by my WebID contain the following relationships:

| <some person> | has name  | Vincent   |
| <some person> | works at  | Inrupt    |
| <some person> | job title | Developer |

(The common terminology is: every row contains a _Statement_, with
in the first column the Statement's _Subject_, in the second column
a _Predicate_, and the _Object_ in the third column.)

You might notice that the table above is a description of
`<some person>`: it's someone whose name is Vincent, who works at
Inrupt and is a developer. However, `<some person>` is not really
usable as a stable and unique identifier: for all we know, someone
else might have a Document elsewhere that uses the exact same
identifier. To solve this problem, Solid uses URLs as unique
identifiers: after all, the Document that describes the entries
already has a URL, 

src/components/Dashboard.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { LogoutButton } from '@solid/react';
+import { foaf } from 'rdf-namespaces';
+import { useProfile } from '../hooks/useProfile';
 
 export const Dashboard: React.FC = () => {
+  const profile = useProfile();
+
+  const name = (profile) ? profile.getLiteral(foaf.name) : null;
+  const greeting = (name)
+    ? `Hi ${name}`
+    : 'Hi';
+
   return <>
-    <section className="section">You are logged in! You will be able to do things here.</section>
+    <section className="section">
+      {greeting}
+    </section>
     <footer className="footer has-text-right">
       <LogoutButton className="button"/>
     </footer>

Vinnl/notepod

src/components/Dashboard.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { LogoutButton } from '@solid/react';
+import { foaf } from 'rdf-namespaces';
+import { useProfile } from '../hooks/useProfile';
 
 export const Dashboard: React.FC = () => {
+  const profile = useProfile();
+
+  const name = (profile) ? profile.getString(foaf.name) : null;
+  const greeting = (name)
+    ? `Hi ${name}`
+    : 'Hi';
+
   return <>
-    <section className="section">You are logged in! You will be able to do things here.</section>
+    <section className="section">
+      {greeting}
+    </section>
     <footer className="footer has-text-right">
       <LogoutButton className="button"/>
     </footer>